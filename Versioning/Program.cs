4517f2c1d11a3718d93b7523b630c9f1728af8be
NameInikita <name.inikita@gmail.com>
2017-08-01 23:21:34 +0300

    Замещение метода

---
+++ Versioning/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Versioning
+{
+    class Tree
+    {
+        public void Air() { Console.WriteLine("1"); }
+        public /*virtual*/ void Water() { Console.WriteLine("2"); }
+    }
+
+    class Aple : Tree
+    {
+        public new void Air() { Console.WriteLine("3"); }//Замещение
+        public /*override*/ void Water() { Console.WriteLine("4"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree inst1 = new Aple();
+
+            inst1.Air();//1
+            inst1.Water();//2
+
+            Aple inst2= inst1 as Aple;
+
+            inst2.Air();//3
+            inst2.Water();//4
+
+            Console.ReadKey();
+        }
+    }
+}
